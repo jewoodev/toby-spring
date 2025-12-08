5c2a9ea281b3307f01c2fde4d4e408e654e8f70d
jewoodev <jewoos15@naver.com>
2025-12-08 12:57:12 +0900

    🔨 refactor: 관심사의 분리

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -14,22 +14,15 @@ import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
 public class PaymentService {
+    public static void main(String[] args) throws IOException {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment);
+    }
+
     public Payment prepare(Long orderId, String currency, BigDecimal foriegnCurAmount) throws IOException {
-        // 환율 가져오기
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        var con = (HttpURLConnection) url.openConnection();
-        var br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
-
-        var om = new ObjectMapper();
-        var exRateData = om.readValue(response, ExRateData.class);
-        BigDecimal krwRate = exRateData.rates().get("KRW");
-
-        // 금액 계산
+        BigDecimal krwRate = getExRate(currency);
         BigDecimal convertedAmount = foriegnCurAmount.multiply(krwRate);
-
-        // 유효 시간 계산
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
         var paymentDecimal = PaymentDecimal.builder()
@@ -41,9 +34,16 @@ public class PaymentService {
         return new Payment(orderId, currency, paymentDecimal, validUntil);
     }
 
-    public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment);
+    private BigDecimal getExRate(String currency) throws IOException {
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        var con = (HttpURLConnection) url.openConnection();
+        var br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+
+        var om = new ObjectMapper();
+        var exRateData = om.readValue(response, ExRateData.class);
+        BigDecimal krwRate = exRateData.rates().get("KRW");
+        return krwRate;
     }
 }
