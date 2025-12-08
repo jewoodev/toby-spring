db8b8ecc0bad21b27a8cc237738784078e8aaaa4
jewoodev <jewoos15@naver.com>
2025-12-08 13:06:15 +0900

    🔨 refactor: 상속을 통한 확장

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -1,32 +1,19 @@
 package tobyspring.hellospring;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import tobyspring.hellospring.dto.ExRateData;
 import tobyspring.hellospring.vo.PaymentDecimal;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
 
-public class PaymentService {
-    public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment);
-    }
-
-    public Payment prepare(Long orderId, String currency, BigDecimal foriegnCurAmount) throws IOException {
+abstract public class PaymentService {
+    public Payment prepare(Long orderId, String currency, BigDecimal foriegnCurrencyAmount) throws IOException {
         BigDecimal krwRate = getExRate(currency);
-        BigDecimal convertedAmount = foriegnCurAmount.multiply(krwRate);
+        BigDecimal convertedAmount = foriegnCurrencyAmount.multiply(krwRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
         var paymentDecimal = PaymentDecimal.builder()
-                .foriegnCurAmount(foriegnCurAmount)
+                .foriegnCurAmount(foriegnCurrencyAmount)
                 .exRate(krwRate)
                 .convertedAmount(convertedAmount)
                 .build();
@@ -34,16 +21,5 @@ public class PaymentService {
         return new Payment(orderId, currency, paymentDecimal, validUntil);
     }
 
-    private BigDecimal getExRate(String currency) throws IOException {
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        var con = (HttpURLConnection) url.openConnection();
-        var br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
-
-        var om = new ObjectMapper();
-        var exRateData = om.readValue(response, ExRateData.class);
-        BigDecimal krwRate = exRateData.rates().get("KRW");
-        return krwRate;
-    }
+    abstract public BigDecimal getExRate(String currency) throws IOException;
 }
---
+++ src/main/java/tobyspring/hellospring/WebApiExRatePaymentService.java
@@ -0,0 +1,28 @@
+package tobyspring.hellospring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import tobyspring.hellospring.dto.ExRateData;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class WebApiExRatePaymentService extends PaymentService {
+    @Override
+    public BigDecimal getExRate(String currency) throws IOException {
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        var con = (HttpURLConnection) url.openConnection();
+        var br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+
+        var om = new ObjectMapper();
+        var exRateData = om.readValue(response, ExRateData.class);
+        BigDecimal krwRate = exRateData.rates().get("KRW");
+        return krwRate;
+    }
+}
