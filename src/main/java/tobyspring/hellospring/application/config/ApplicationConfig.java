43645d7de28cb3f9e471dd201ec27b28e9e8d7c1
jewoodev <jewoos15@naver.com>
2025-12-22 15:52:37 +0900

    🔨 refactor: @Transactional과 AOP

--- src/main/java/tobyspring/hellospring/application/config/ApplicationConfig.java
+++ src/main/java/tobyspring/hellospring/application/config/ApplicationConfig.java
@@ -2,15 +2,15 @@ package tobyspring.hellospring.application.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tobyspring.hellospring.application.required.order.OrderRepository;
 import tobyspring.hellospring.application.service.OrderService;
 import tobyspring.hellospring.application.service.OrderServiceImpl;
-import tobyspring.hellospring.application.service.OrderServiceTxProxy;
 import tobyspring.hellospring.application.service.PaymentService;
 import tobyspring.hellospring.domain.payment.ExRateProvider;
 
 @Configuration
+@EnableTransactionManagement
 public class ApplicationConfig {
     @Bean
     public PaymentService paymentService(ExRateProvider exRateProvider) {
@@ -18,10 +18,7 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public OrderService orderService(OrderRepository orderRepository, PlatformTransactionManager transactionManager) {
-        return new OrderServiceTxProxy(
-                new OrderServiceImpl(orderRepository),
-                transactionManager
-        );
+    public OrderService orderService(OrderRepository orderRepository) {
+        return new OrderServiceImpl(orderRepository);
     }
 }
--- src/main/java/tobyspring/hellospring/application/service/OrderServiceImpl.java
+++ src/main/java/tobyspring/hellospring/application/service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package tobyspring.hellospring.application.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tobyspring.hellospring.application.required.order.OrderRepository;
 import tobyspring.hellospring.application.service.vo.OrderRequest;
 import tobyspring.hellospring.domain.order.Order;
@@ -16,6 +17,7 @@ public class OrderServiceImpl implements OrderService {
         this.orderRepository = orderRepository;
     }
 
+    @Transactional
     @Override
     public Order createOrder(String orderName, BigDecimal totalAmount) {
         var order = new Order(orderName, totalAmount);
@@ -24,6 +26,7 @@ public class OrderServiceImpl implements OrderService {
         return order;
     }
 
+    @Transactional
     @Override
     public List<Order> createOrders(List<OrderRequest> requests) {
         return requests.stream().map(request ->
